b728170970d25f0d01911d062861f8d093af8b87
XLMaverick <yangxl0319@163.com>
2019-05-23 21:09:28 +0800

    add 5th 6th

---
+++ vslam_fourteen_lectures/undistort/build/CMakeFiles/undistort.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/undistort.dir/undistort_image.cpp.o"
+  "undistort.pdb"
+  "undistort"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/undistort.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
